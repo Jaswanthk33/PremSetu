06ab275977396f01515d5d6b3e5b1b4ba00b4fd4
Jaswanth Kandati <jaswanth.kandati@gmail.com>
2025-08-07 02:32:25 +0530

    changed name in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/baby-memory-magic/' : '/',
+  base: mode === 'production' ? '/PremSetu/' : '/',
   server: {
     host: "::",
     port: 8080,
